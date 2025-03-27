ca1e588a709c7f5d1c7a3b05a2983b14b19433b5
Alexandre Cholat <165085048+Alexandre-Cholat@users.noreply.github.com>
2025-03-27 09:44:23 +0100

    programme fonctionelle
    joue(tjrs4,_,4).

--- projet-s4-gestionDeJeux.pl
+++ projet-s4-gestionDeJeux.pl
@@ -1,10 +1,10 @@
-/*Joue toujours 4*/
-joue(tjrs4,[],4).
+/*Joue toujours 4 (erreur supprimee*/
+joue(tjrs4,_,4).
 
 /*aleatoire*/
 joue(aleatwar,_,N):-random_between(1,5,N).
 
-/*coup précédent de l'adversaire
+/*coup prÃ©cÃ©dent de l'adversaire
 */
 joue(titfortat,[],N):-random_between(1,5,N).
 joue(titfortat,[[_,C]|_],C).
@@ -16,7 +16,7 @@ gestionDeJeux(Equipe1, Equipe2, NbDeParties, FinalScore1, FinalScore2) :-
 
 jeuxV1(_, _, L, NbDeParties, Score1, Score2, Score1, Score2) :-
     length(L, Z),
-    write('Checking base case: '), write(Z), write(' == '), write(NbDeParties), nl,
+    /*write('Checking base case: '), write(Z), write(' == '), write(NbDeParties), nl,*/
     Z == NbDeParties.
 
 jeuxV1(Equipe1, Equipe2, L, NbDeParties, Score1, Score2, FinalScore1, FinalScore2) :-
@@ -25,8 +25,8 @@ jeuxV1(Equipe1, Equipe2, L, NbDeParties, Score1, Score2, FinalScore1, FinalScore
     write('E1: '), write(E1), write(', E2: '), write(E2), nl,
     E2 + 1 =:= E1,
     NouvScore is E1 + E2 + Score2,
-    write('Case 1: Updating Score2 -> NouvScore: '), write(NouvScore), nl,
-    jeuxV1(Equipe1, Equipe2, [[E1, E2]|L], NbDeParties, Score1, NouvScore, FinalScore1, FinalScore2), !.
+    write('Cas 1: Mise a jour du Score2 -> NouvScore: '), write(NouvScore), nl,
+    jeuxV1(Equipe1, Equipe2, [[E1, E2]|L], NbDeParties, Score1, NouvScore, FinalScore1, FinalScore2).
 
 jeuxV1(Equipe1, Equipe2, L, NbDeParties, Score1, Score2, FinalScore1, FinalScore2) :-
     joue(Equipe1, L, E1),
@@ -34,8 +34,8 @@ jeuxV1(Equipe1, Equipe2, L, NbDeParties, Score1, Score2, FinalScore1, FinalScore
     write('E1: '), write(E1), write(', E2: '), write(E2), nl,
     E1 + 1 =:= E2,
     NouvScore is E1 + E2 + Score1,
-    write('Case 2: Updating Score1 -> NouvScore: '), write(NouvScore), nl,
-    jeuxV1(Equipe1, Equipe2, [[E1, E2]|L], NbDeParties, NouvScore, Score2, FinalScore1, FinalScore2), !.
+    write('Cas 2: Mise a jour du Score1 -> NouvScore: '), write(NouvScore), nl,
+    jeuxV1(Equipe1, Equipe2, [[E1, E2]|L], NbDeParties, NouvScore, Score2, FinalScore1, FinalScore2).
 
 jeuxV1(Equipe1, Equipe2, L, NbDeParties, Score1, Score2, FinalScore1, FinalScore2) :-
     joue(Equipe1, L, E1),
@@ -43,5 +43,4 @@ jeuxV1(Equipe1, Equipe2, L, NbDeParties, Score1, Score2, FinalScore1, FinalScore
     write('E1: '), write(E1), write(', E2: '), write(E2), nl,
     S1 is Score1 + E1,
     S2 is Score2 + E2,
-    write('Case 3: Updating both scores -> S1: '), write(S1), write(', S2: '), write(S2), nl,
-    jeuxV1(Equipe1, Equipe2, [[E1, E2]|L], NbDeParties, S1, S2, FinalScore1, FinalScore2).
+    write('Cas 3: Mise a jour des scores -> S1: '), write(S1), write(', S2: '), write(S2), nl, jeuxV1(Equipe1, Equipe2, [[E1, E2]|L], NbDeParties, S1, S2, FinalScore1, FinalScore2).
